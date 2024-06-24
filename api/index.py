65d5dc04d9cdd6e48a5ecb12e9486775070fe86a
faisalach <faisalachramsyah@gmail.com>
2024-06-24 17:22:38 +0700

    add cors

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
-from flask import Flask
+import os
+from flask import Flask, request,jsonify,send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
