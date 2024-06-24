dfdd5d64b4d040cbfafe03e3a0d25805ead1914a
faisalach <faisalachramsyah@gmail.com>
2024-06-24 17:26:21 +0700

    import model

--- api/index.py
+++ api/index.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request,jsonify,send_from_directory
 from flask_cors import CORS
 
+from model_food import recommend_foods
+from model_stunting import predict_status_gizi
+
 app = Flask(__name__)
 CORS(app)
 
